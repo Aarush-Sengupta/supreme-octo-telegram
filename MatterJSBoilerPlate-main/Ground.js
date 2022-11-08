357ae49a42ec3d6caeffabe53eb412e830ba769f
Aarush-Sengupta <105724875+Aarush-Sengupta@users.noreply.github.com>
2022-11-08 17:10:38 +0400

    Add files via upload

---
+++ MatterJSBoilerPlate-main/Ground.js
@@ -0,0 +1,32 @@
+class Ground{
+    constructor(x,y,w,h){
+        var option={
+            isStatic:true
+        }
+        this.body=Bodies.rectangle(x,y,w,h,option)
+        this.w=w;
+        this.h=h;
+        World.add(world, this.body);
+    }
+    display(){
+
+      var pos=this.body.position;
+      push();
+            translate(pos.x, pos.y);
+			rectMode(CENTER)
+			strokeWeight(4);
+			fill(255,255,0)
+			rect(0,0,this.w, this.h);
+
+      pop();
+
+
+
+        
+    }
+
+
+
+
+
+}
